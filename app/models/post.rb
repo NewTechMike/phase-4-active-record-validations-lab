b29ce113410d61bf9a966d26e48bc214cff40a83
Michael Harris <xero5k@yahoo.com>
2022-02-23 20:30:10 -0600

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  validate :clickbait
+
+  CLICKABLE = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait
+    if CLICKABLE.none?{ |search| search.match title}
+      errors.add(:title, "this is not clickbait")
+    end
+  end 
 end
+
